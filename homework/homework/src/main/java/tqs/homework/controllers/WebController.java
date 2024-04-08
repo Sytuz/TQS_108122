3fd7054829db79cc728f5ebdf9807ca08fd05a43
Alex <71047866+Sytuz@users.noreply.github.com>
2024-04-08 16:49:58 +0100

    HW - Home Page (Static)

---
+++ homework/homework/src/main/java/tqs/homework/controllers/WebController.java
@@ -0,0 +1,13 @@
+package tqs.homework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+   @GetMapping("/home")
+   public String home() {
+      return "home";
+   }
+}
