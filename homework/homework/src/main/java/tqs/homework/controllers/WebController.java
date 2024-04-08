6d3f13307e3b0d5b1490eebd4fe6f386d5f891af
Alex <71047866+Sytuz@users.noreply.github.com>
2024-04-08 23:44:56 +0100

    HW - Reservation and Trip Pages (Static)

--- homework/homework/src/main/java/tqs/homework/controllers/WebController.java
+++ homework/homework/src/main/java/tqs/homework/controllers/WebController.java
@@ -2,6 +2,9 @@ package tqs.homework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class WebController {
@@ -9,5 +12,20 @@ public class WebController {
    @GetMapping("/home")
    public String home() {
       return "home";
+   }
+
+   @PostMapping("/reservation")
+   public String reservation(@RequestParam(value = "code", required = true) String code) {
+      return "reservation";
+   }
+
+   @PostMapping("/cancel")
+   public String cancel(@RequestParam(value = "altCode", required = true) String altCode) {
+      return "cancel";
+   }
+
+   @GetMapping("/trip")
+   public String trip(@RequestParam(value = "code", required = true) String code) {
+      return "trip";
    }
 }
